e2cff73b360572e1991d3a489ee69f8dbda16424
Justin Patriquin <justin@sidestory.email>
2018-01-24 16:10:18 -0400

    update

--- UnityProject/Assets/OpenMined/Config/Config.cs
+++ UnityProject/Assets/OpenMined/Config/Config.cs
@@ -3,9 +3,8 @@ using System.Collections;
 
 namespace OpenMined.Config {
     public static class Config {
-        public static string bygoneServer = "ec2-54-84-21-224.compute-1.amazonaws.com";
-        public static bool trainer = true;
+        public static string bygoneServer = "localhost";
+        public static bool trainer = false;
         public static bool identity = false;
     }
 }
-
